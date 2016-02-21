58bd04cb19ccd76039dc171edf4dcbb788b1ad76
maegan <maegan.burke@gmail.com>
2016-02-21 11:45:49 -0500

    good for mobile except h1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   get '/' => 'home#index'
+  get '/mobile' => 'home#mobile'
 
   get '/subscribe' => 'emailapi#index'
   post '/subscribe' => 'emailapi#subscribe'
